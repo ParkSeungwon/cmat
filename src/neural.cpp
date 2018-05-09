bd7dadd2f403e5aaa4fa8a9b4830faacccdfc60c
zezeon <zezeon@msn.com>
2018-05-09 12:36:44 -0700

    0.9

--- src/neural.cpp
+++ src/neural.cpp
@@ -1,6 +1,10 @@
 #include"neural.h"
 #include<iostream>
 using namespace std;
+float relu(float x) {
+	if(x > 0 ) return x;
+	else return 0;
+}
 
 int main()
 {
@@ -12,6 +16,9 @@ int main()
 	uniform_real_distribution<float> di{-1, 1};
 	net.init(di);
 	cout << weight<0>(net) << weight<1>(net);
-	net.feed_forward({1,1,1});
+//	net.transfer_function(relu);
+	net.feed_forward({0.1,0.4,1});
+	net.back_propagation({1,1,0,0,1});
+	cout << weight<0>(net) << weight<1>(net);
 	cout << layer<0>(net) << layer<1>(net) << layer<2>(net);
 }
--- src/neural.h
+++ src/neural.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<random>
+#include<functional>
+#include<algorithm>
 #include"base.h"
 
 template<unsigned Idx, unsigned W, unsigned H> class Weight {
@@ -52,21 +54,55 @@ public:
 			init<Di, Idx + 1>(di);
 		}
 	}
+	void transfer_function(std::function<float(float)> f) { transfer_function_ = f; }
 	auto feed_forward(std::vector<float> v) {
 		for(int i=0; i<v.size(); i++) layer<0>(*this)[0][i] = v[i];
-		forward_feed();
+		feed_forward();
 		return layer<sizeof...(Ns) - 1>(*this);
+	}
+	float back_propagation(std::vector<float> v, float alpha = 0.1) {
+		float err = 0;
+		auto& d = delta<sizeof...(Ns) - 1>(*this);
+		for(int i = 0; i < d.height_; i++) {
+			float o = layer<sizeof...(Ns) - 1>(*this)[0][i];
+			d[0][i] = (v[i] - o) * o * ( 1 - o);
+			err += (v[i] - o ) * (v[i] - o);
+		}
+		calc_delta();
+		update_weight(alpha);
+		return 0.5 * err;
 	}
 
 private:
 	std::random_device rd_;
-	std::function<float(float)> transfer = [](float x) {return 1 / (1 + exp(-x));};
-	template<unsigned Idx = 0> void forward_feed() {
+	std::function<float(float)> transfer_function_
+		= [](float x) {return 1 / (1 + exp(-x));};//sigmoid default
+	template<unsigned Idx = 0> void feed_forward() {
 		if constexpr(Idx < sizeof...(Ns) - 1) {
-			auto a = weight<Idx>(*this) * layer<Idx>(*this);
-			for(int i=0; i<a.height_; i++) a[0][i] = transfer(a[0][i]);
-			layer<Idx + 1>(*this) = a;
-			forward_feed<Idx + 1>();
+			layer<Idx + 1>(*this) = weight<Idx>(*this) * layer<Idx>(*this);
+			float* p = reinterpret_cast<float*>(layer<Idx + 1>(*this).arr_.data());
+			transform(p, p + layer<Idx + 1>(*this).height_, p, transfer_function_);
+			feed_forward<Idx + 1>();
+		}
+	}
+	template<unsigned Idx = sizeof...(Ns) - 1> void calc_delta() {
+		if constexpr(Idx > 0) {
+			const auto& w = weight<Idx - 1>(*this);
+			for(int i=0; i<w.width_; i++) {
+				float o = layer<Idx>(*this)[0][i], sum = 0;
+				for(int j=0; j<w.height_; j++)
+					sum += w[i][j] * delta<Idx>(*this)[0][j];
+				delta<Idx - 1>(*this)[0][i] = sum * o * (1 - o);
+			}
+			calc_delta<Idx - 1>();
+		}
+	}
+	template<unsigned Idx = 0> void update_weight(float a) {
+		if constexpr(Idx < sizeof...(Ns) - 1) {
+			auto& w = weight<Idx>(*this);
+			for(int i=0; i<w.width_; i++) for(int j=0; j<w.height_; j++) 
+				w[i][j] += a * delta<Idx + 1>(*this)[0][i] * layer<Idx>(*this)[0][j];
+			update_weight<Idx + 1>(a);
 		}
 	}
 };
